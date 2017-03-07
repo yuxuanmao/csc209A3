949472608c65da654e6ea53491a4222da9776c22
Karen Reid <reid@cs.toronto.edu>
2017-03-06 23:24:22 -0500

    Adding starter code

---
+++ ex8/checkpasswd.c
@@ -0,0 +1,40 @@
+/* this solution needs error checking! */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/* Read a user id and password from standard input, 
+   - create a new process to run the validate program
+   - send 'validate' the user id and password on a pipe, 
+   - print a message 
+        "Password verified" if the user id and password matched, 
+        "Invalid password", or 
+        "No such user"
+     depending on the return value of 'validate'.
+*/
+
+/* Use the exact messages defined below in your program." */
+
+#define VERIFIED "Password verified\n"
+#define BAD_USER "No such user\n"
+#define BAD_PASSWORD "Invalid password\n"
+#define OTHER "Error validating password\n"
+
+
+int main(void) {
+    char userid[10];
+    char password[10];
+
+    /* Read a user id and password from stdin */
+    printf("User id:\n");
+    scanf("%s", userid);
+    printf("Password:\n");
+    scanf("%s", password);
+
+	
+
+    return 0;
+}
